40b6c473fcc5e6913a76c704fe894198110994a9
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-23 12:16:57 -0300

    Cambiado el nombre de la función

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-var assert = require('assert');
+const assert = require('assert');
 var app = require('../src/app');
-describe('funcionFiltro', function() {
+describe('verifyFilter', function() {
   it('debe retornar true si se le entrega un string y un número válido', () => {
-    assert.equal(true, app.funcionParaFiltrar('greyscale', 80));
-    assert.equal(true, app.funcionParaFiltrar('greyscale', 100));
+    assert.equal(true, app.verifyFilter('greyscale', 80));
+    assert.equal(true, app.verifyFilter('greyscale', 100));
   });
 
   it('debe retornar false si se le entrega un string y/o un número fuera del rango', () => {
-    assert.equal(false, app.funcionParaFiltrar('potato', 100));
-    assert.equal(false, app.funcionParaFiltrar('potato', 200));
-    assert.equal(false, app.funcionParaFiltrar('greyscale', -100));
+    assert.equal(false, app.verifyFilter('potato', 100));
+    assert.equal(false, app.verifyFilter('potato', 200));
+    assert.equal(false, app.verifyFilter('greyscale', -100));
   });
-})
+});

c4a956e55bf47bb32c78d88e6ff9d60674b83319
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-28 21:29:24 -0300

    Añadido el test para verificar la función replaceNonNumbers

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 var app = require('../src/app');
+
 describe('verifyFilter', function() {
   it('debe retornar true si se le entrega un string y un número válido', () => {
     assert.equal(true, app.verifyFilter('greyscale', 80));
@@ -11,4 +12,20 @@ describe('verifyFilter', function() {
     assert.equal(false, app.verifyFilter('potato', 200));
     assert.equal(false, app.verifyFilter('greyscale', -100));
   });
+});
+
+describe('Verificar que value es un valor que sólo contiene números', () => {
+  it('Debe retornar true si el string contiene sólo valores numéricos', () => {
+    assert.equal('100', app.replaceNonNumbers('100'));
+    assert.equal('0', app.replaceNonNumbers('0'));
+    assert.equal('99', app.replaceNonNumbers('99'));
+    assert.equal('30.5', app.replaceNonNumbers('30.5'));
+    assert.equal('-50', app.replaceNonNumbers('-50'));
+    assert.equal('100%', app.replaceNonNumbers('100%'));
+  });
+
+  it('Debe retornar false si el string contiene una letra', () => {
+    assert.equal('10', app.replaceNonNumbers('1o0'));
+    assert.equal('14', app.replaceNonNumbers('14ñ'));
+  })
 });

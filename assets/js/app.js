6a0bd411000054591c6b5c090ee5473dc3c72692
Helvette <malastrugo@live.cl>
2018-02-01 11:41:33 -0300

    actualización demo, todos los botones y efectos funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-  objFunctions.limitWidthImg('img', '100%');
-  objFunctions.changeHeightImg('img', 'auto', true);
+  objFunctions.limitWidthImg('section img', '100%');
+  objFunctions.changeHeightImg('section img', 'auto', true);
   objFunctions.hoverLargeScale('section.zoom-in figure', '1.3');
   objFunctions.hoverNormalScale('section.zoom-out figure', '1.4');
   objFunctions.applyOpacity('section.opacity figure','.7');
   objFunctions.applyColorOpacity('section.color-opacity figure', '#7D1935', '.5');
   objFunctions.hoverSlide('section.slide figure', '1.3', '30');
-  objFunctions.hoverCardify('section.alt-text figure', '#7D1935')
+  objFunctions.hoverCardify('section.alt-text figure', '#7D1935');
+  objFunctions.applyFilteryHover('section.bw-filter figure img:not(.severla)', 'grayscale', '100');
+  objFunctions.removeFilteryHover('section.bw-filter figure .severla', 'grayscale', '100');
+  objFunctions.applyFilteryHover('section.sepia-filter figure img', 'sepia', '100');
+    objFunctions.removeFilteryHover('section.sepia-filter figure .severla', 'sepia', '100');
   $('section').hide();
   $('section.zoom-in').show();
 
@@ -31,6 +35,12 @@ $(document).ready(function() {
     }
     if ($(this).hasClass('slide')) {
       $('section.slide').show();
+    }
+    if ($(this).hasClass('bw-filter')) {
+      $('section.bw-filter').show();
+    }
+    if ($(this).hasClass('sepia-filter')) {
+      $('section.sepia-filter').show();
     }
     if ($(this).hasClass('alt-text')) {
       $('section.alt-text').show();

0d8cd7a1de4ac92f51a2345d525793216fe5d908
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-02 06:48:09 -0300

    Creando funciones para mostrar la documentación

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
   objFunctions.limitWidthImg('#logo', '100%');
+  showDoc('.selector');
 });
+
+const showDoc = selector => {
+  $(selector).click(function() {
+    let thisData = $(this).html().toLowerCase();
+    $('.card-header').html(`${documentation[thisData].title}`);
+    $('.card-body').html(`<p class="description">${documentation[thisData].description}</p>
+      <code class="javascript">${documentation[thisData].function}</code>
+      <p class="description">${documentation[thisData].use}</p>
+      <p class="description important">${documentation[thisData].important}</p>
+      ${documentation[thisData].example}`);
+  });
+};
